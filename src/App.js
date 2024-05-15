99175959452a71b9ef3afcfe53d15f65d956badf
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-05-15 09:50:57 +0900

    feat: add todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { element, render } from "./view/html-util.js";
 console.log("App.js loaded");
 export class App {
 	mount() {
 		const formElement = document.getElementById("js-form");
 		const inputElement = document.getElementById("js-form-input");
+		const containerElement = document.getElementById("js-todo-list");
+		const todoItemCountElement = document.getElementById("js-todo-count");
+		const todoListElement = element`<ul><ul>`;
+		let todoItemCount = 0;
 		formElement.addEventListener("submit", (event) => {
 			event.preventDefault();
-			console.log(`入力欄の値: ${inputElement.value}`);
+			const todoItemElement = element`<li>${inputElement.value}</li>`;
+			todoListElement.appendChild(todoItemElement);
+			render(todoListElement, containerElement);
+			++todoItemCount;
+			todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+			inputElement.value = "";
 		});
 	}
 }
